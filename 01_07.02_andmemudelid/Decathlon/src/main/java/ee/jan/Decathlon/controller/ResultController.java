77a50b5004a8faa0c6b8179180fd2176a797312e
Jan Aaron Einloo <janeinlo@tlu.ee>
2025-02-27 23:26:23 +0200

    Viimane

--- 01_07.02_andmemudelid/Decathlon/src/main/java/ee/jan/Decathlon/controller/ResultController.java
+++ 01_07.02_andmemudelid/Decathlon/src/main/java/ee/jan/Decathlon/controller/ResultController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 
@@ -78,6 +79,7 @@ public class ResultController {
         resultRepository.save(result);
         return resultRepository.findAll();
     }
+
 
     @DeleteMapping("results/{id}")
     public List<Result> deleteResult(@PathVariable Long id) {
